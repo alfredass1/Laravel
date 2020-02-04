4fdaf40839c6c8b8f4cfb311b59fc630d241dbc6
Aivrpsk <destrocto@yahoo.com>
2020-02-04 13:47:26 +0200

    commit

--- resources/views/skelbimai/_partials/nav.blade.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="#">Linkas</a></li>
-        <li><a href="#">Linkas1</a></li>
-        <li><a href="#">Linkas2</a></li>
-        <li><a href="#">Linkas3</a></li>
-    </ul>
-</nav>
--- resources/views/skelbimai/pages/profile.blade.php
+++ resources/views/skelbimai/pages/profile.blade.php
@@ -1,8 +1,5 @@
 @extends('skelbimai/main')
 
 @section('content')
-    <main>
-        <h2>Profilis</h2>
-        <p>Jau greitai</p>
-    </main>
+
 @stop
